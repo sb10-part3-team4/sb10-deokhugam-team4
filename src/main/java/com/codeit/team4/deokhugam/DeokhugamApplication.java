52b4aabc14625f9d3cebb3823653db65978e9f81
dustle <pwdustle@gmail.com>
2026-04-14 01:07:09 +0900

    first commit

---
+++ src/main/java/com/codeit/team4/deokhugam/DeokhugamApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.team4.deokhugam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokhugamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeokhugamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/team4/deokhugam/DeokhugamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.team4.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokhugamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
