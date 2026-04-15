c5bc6be5c991d4bfb19e2f152d9362ce35706d13
JeongSooHyeon <jsh75721@naver.com>
2026-04-15 17:43:04 +0900

    feat: BookRepository 작성

---
+++ src/main/java/com/codeit/team4/deokhugam/book/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.codeit.team4.deokhugam.book.repository;
+
+import com.codeit.team4.deokhugam.book.entity.Book;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    Optional<Book> findByIdAndDeletedAtIsNull(UUID uuid);
+    boolean existsByIsbnAndDeletedAtIsNull(String isbn);
+
+    // 도서 목록 조회는 JOOQ로 구현
+
+}
