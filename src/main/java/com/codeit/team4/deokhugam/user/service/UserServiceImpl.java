c5fe48f44867641fa2d9a20e787856030cf56a57
zinuzanu <wsx104301@naver.com>
2026-04-17 14:11:22 +0900

    feat: 사용자 로그인 구현 및 테스트 코드 작성

--- src/main/java/com/codeit/team4/deokhugam/user/service/UserServiceImpl.java
+++ src/main/java/com/codeit/team4/deokhugam/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.codeit.team4.deokhugam.user.service;
 
 import com.codeit.team4.deokhugam.global.error.BusinessException;
 import com.codeit.team4.deokhugam.global.error.ErrorCode;
+import com.codeit.team4.deokhugam.user.dto.UserLoginRequest;
 import com.codeit.team4.deokhugam.user.dto.UserRegisterRequest;
 import com.codeit.team4.deokhugam.user.dto.UserResponse;
 import com.codeit.team4.deokhugam.user.entity.User;
@@ -44,6 +45,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public UserResponse loginUser(UserLoginRequest request) {
+
+        User user = findActiveUserByEmail(request.email());
+        validatePassword(user, request.password());
+
+        log.info("로그인 성공: userId={}", user.getId());
+
+        return userMapper.toResponse(user);
+    }
+
+    @Override
     public User findById(UUID userId) {
         return userRepository.findByIdAndDeletedAtIsNull(userId)
                 .orElseThrow(() -> new BusinessException(
@@ -55,5 +68,17 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsByEmailAndDeletedAtIsNull(email)) {
             throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
         }
+    }
+
+    private void validatePassword(User user, String password) {
+        if (!user.getPassword().equals(password)) {
+            throw new BusinessException(ErrorCode.INVALID_PASSWORD);
+        }
+    }
+
+    private User findActiveUserByEmail(String email) {
+        return userRepository.findByEmailAndDeletedAtIsNull(email)
+                .orElseThrow(() -> new BusinessException(
+                        ErrorCode.USER_NOT_FOUND, "email=" + email));
     }
 }
