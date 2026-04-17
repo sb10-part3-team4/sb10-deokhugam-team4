8ac7cd93bc4d851fa214c0d9f6c3151387076bb5
jiho0420 <jiho0420@gachon.ac.kr>
2026-04-17 14:49:14 +0900

    chore: Comment 엔티티에 index 문서화 추가

--- src/main/java/com/codeit/team4/deokhugam/comment/entity/Comment.java
+++ src/main/java/com/codeit/team4/deokhugam/comment/entity/Comment.java
@@ -6,6 +6,7 @@ import com.codeit.team4.deokhugam.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -15,7 +16,14 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "comments")
+@Table(
+        name = "comments",
+        indexes = {
+                @Index(name = "idx_comments_review_id", columnList = "review_id"),
+                @Index(name = "idx_comments_user_id", columnList = "user_id"),
+                @Index(name = "idx_comments_deleted_at", columnList = "deleted_at"),
+                @Index(name = "idx_comments_created_at", columnList = "created_at")
+        })
 @NoArgsConstructor
 public class Comment extends BaseUpdatableEntity {
     @ManyToOne(fetch = FetchType.LAZY) // N+1 방지를 위해 항상 LAZY
