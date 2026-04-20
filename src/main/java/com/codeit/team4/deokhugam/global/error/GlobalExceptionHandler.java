d64245df340939837f616d429d155b186fadd655
dustle <pwdustle@gmail.com>
2026-04-20 15:37:03 +0900

    feat: 헤더 요청 누락 예외를 400 에러 핸들러에 추가

--- src/main/java/com/codeit/team4/deokhugam/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/team4/deokhugam/global/error/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -49,6 +50,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({
+            MissingRequestHeaderException.class,
             MissingServletRequestParameterException.class,
             HttpMessageNotReadableException.class,
             MethodArgumentTypeMismatchException.class
