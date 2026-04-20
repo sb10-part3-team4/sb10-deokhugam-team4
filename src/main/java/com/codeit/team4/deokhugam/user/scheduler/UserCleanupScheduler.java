df487f6ab441ca96921fdd18accd3141cf16d665
zinuzanu <wsx104301@naver.com>
2026-04-20 11:43:13 +0900

    feat: 스케줄러 초기 설정 구현

---
+++ src/main/java/com/codeit/team4/deokhugam/user/scheduler/UserCleanupScheduler.java
@@ -0,0 +1,20 @@
+package com.codeit.team4.deokhugam.user.scheduler;
+
+import com.codeit.team4.deokhugam.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserCleanupScheduler {
+
+    private final UserService userService;
+
+    @Scheduled(fixedRate = 300000)
+    public void runCleanup() {
+        userService.deleteExpiredUsers();
+    }
+}
