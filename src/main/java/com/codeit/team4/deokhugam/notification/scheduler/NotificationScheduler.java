30083024db5424c1fd0c4040ce8f356435c70905
zinuzanu <wsx104301@naver.com>
2026-04-23 16:49:18 +0900

    refactor: zone 중복 제거 및 분산락 TODO 추가

--- src/main/java/com/codeit/team4/deokhugam/notification/scheduler/NotificationScheduler.java
+++ src/main/java/com/codeit/team4/deokhugam/notification/scheduler/NotificationScheduler.java
@@ -4,7 +4,6 @@ import com.codeit.team4.deokhugam.notification.service.NotificationService;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,13 +14,11 @@ public class NotificationScheduler {
 
     private final NotificationService notificationService;
 
-    @Value("${notification.batch.zone}")
-    private String zone;
-
+    //TODO: Lock 추가 후 개선 예정
     @Scheduled(cron = "${notification.batch.cron}", zone = "${notification.batch.zone}")
     public void runNotificationBatch() {
         Instant now = Instant.now();
-        log.info("알림 삭제 스케줄러 시작: now={}, zone={}", now, zone);
+        log.info("알림 삭제 스케줄러 시작: now={}", now);
         try {
             notificationService.deleteExpiredNotifications();
             log.info("알림 삭제 스케줄러 완료");
