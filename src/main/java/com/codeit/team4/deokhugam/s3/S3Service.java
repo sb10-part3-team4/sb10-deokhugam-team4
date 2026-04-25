d50d419f2a349533c25c0e28fe7720a101bb20b4
jiho0420 <jiho0420@gachon.ac.kr>
2026-04-25 16:40:18 +0900

    feat: 로그 파일 저장 및 업로드 로직 구현

--- src/main/java/com/codeit/team4/deokhugam/s3/S3Service.java
+++ src/main/java/com/codeit/team4/deokhugam/s3/S3Service.java
@@ -1,5 +1,6 @@
 package com.codeit.team4.deokhugam.s3;
 
+import java.io.File;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
@@ -7,4 +8,6 @@ public interface S3Service {
     String upload(MultipartFile file);
 
     void delete(String fileUrl);
+
+    String uploadFile(File file, String dirName);
 }
