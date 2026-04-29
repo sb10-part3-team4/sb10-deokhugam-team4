bc1888db2600545cd222459df373eaaed99365b4
dustle <pwdustle@gmail.com>
2026-04-29 11:09:24 +0900

    feat: main <- dev (#251)
    * refact: Book 관련 코드 리팩토링
    
    - S3 업로드 실패 시 즉시 예외처리 및 삭제 조치
    - Book 생성자, update 오버로딩 하나로 통일
    - searchByIsbn에 Transctioanl 애너테이션 제거
    - 테스트 코드에 Book 생성자 관련 오류 수정
    
    * chore: prod.yaml step 추가
    
    * feat: CloudWatchMetricsConfig (#247)
    
    * refactor: 알림 이벤트 비동기 처리 구조로 리펙터링
    
    * fix: 인기 리뷰 알림 정책을 top10 기준으로 수정
    
    * chore: DisplayName과 메서드명 변경
    
    * refactor: NotificationEvent recover 로그 구조화 및 prefix 통일
    
    * fix: ThreadPoolTaskExecutor 거절 정책 설정으로 알림 유실 방지
    
    ---------
    
    Co-authored-by: JeongSooHyeon <jsh75721@naver.com>
    Co-authored-by: JeongSooHyeon <82567002+JeongSooHyeon@users.noreply.github.com>
    Co-authored-by: zinuzanu <wsx104301@naver.com>

---
+++ src/main/java/com/codeit/team4/deokhugam/global/config/AsyncConfig.java
@@ -0,0 +1,30 @@
+package com.codeit.team4.deokhugam.global.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(10);   // 기본 스레드
+        executor.setMaxPoolSize(30);    // 최대 확장
+        executor.setQueueCapacity(100);  // 대기 큐
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.setThreadNamePrefix("async-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
+
+        executor.initialize();
+        return executor;
+    }
+}
